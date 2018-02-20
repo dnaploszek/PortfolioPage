7f53f9c60b3417dc5844a3ab1d41d09834028b80
Dariusz Naploszek <dariusznaploszek@gmail.com>
2018-02-20 23:49:47 +0100

    Renamed home view and added about view component

---
+++ src/Views/AboutView/AboutView.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import './AboutView.css';
+
+export default class AboutView extends React.Component {
+  render() {
+    return (
+      <div className="about-view--container">
+      </div>
+    );
+  }
+}
--- src/Views/HomeView/HomeView.tsx
+++ src/Views/HomeView/HomeView.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
 
 import ParticleCanvasBackground from '../../Components/ParticleCanvasBackground/ParticleCanvasBackground';
-import './HomeScreen.css';
+import './HomeView.css';
 import HText from '../../Components/UI/HighlightedText/HighlightedText';
 
 const MAX_PARTICLES = 2500;
 
-export default class HomeScreen extends React.Component {
+export default class HomeView extends React.Component {
   render() {
     return (
-      <div className="home-screen--container">
+      <div className="home-view--container">
         <ParticleCanvasBackground
-          className="home-screen--background"
+          className="home-view--background"
           maxParticles={MAX_PARTICLES}
         />
-        <div className="home-screen--inner-container">
-          <div className="home-screen--welcome-container font-header white-color">
+        <div className="home-view--inner-container">
+          <div className="home-view--welcome-container font-header white-color">
             <p className="text-bold font-size-banner">
               Hi!
             </p>
@@ -23,10 +23,10 @@ export default class HomeScreen extends React.Component {
               My name is <HText>Darek</HText> and i'm a <HText>JavaScript Software Developer</HText>.
             </p>
           </div>
-          <div className="home-screen--learn-more-container font-text action-color">
-            <a href="#" className="home-screen--learn-more font-size-text">
+          <div className="home-view--learn-more-container font-text action-color">
+            <a href="#" className="home-view--learn-more font-size-text">
               Learn more about me
-              <div className="home-screen--arrow-bottom action-color"/>
+              <div className="home-view--arrow-bottom action-color"/>
             </a>
           </div>
         </div>
